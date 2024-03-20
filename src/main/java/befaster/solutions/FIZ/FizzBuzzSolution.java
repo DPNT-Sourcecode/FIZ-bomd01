0299b10962e7f310d8dc0dc0d3e11c15be07f304
sbx_user1051 <sbx_user1051@169.254.20.81>
2024-03-20 14:13:25 +0000

    Wed Mar 20 14:13:25 UTC 2024

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,6 +2,9 @@ package befaster.solutions.FIZ;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FizzBuzzSolution {
 
     public String fizz_buzz(Integer number){
@@ -20,24 +23,24 @@ public class FizzBuzzSolution {
         return number.toString();
     }
 
-    public String fizzBuzz(Integer number) {
-        boolean multiple3 = multipleOf3(number);
-        boolean multiple5 = multipleOf5(number);
-
-        if(multiple3 && multiple5){
-            return "fizz buzz";
-        }
-
-        if(multiple3){
-            return "fizz";
-        }
-
-        if(multiple5){
-            return "buzz";
-        }
-
-        return number.toString();
-    }
+//    public String fizzBuzz(Integer number) {
+//        boolean multiple3 = multipleOf3(number);
+//        boolean multiple5 = multipleOf5(number);
+//
+//        if(multiple3 && multiple5){
+//            return "fizz buzz";
+//        }
+//
+//        if(multiple3){
+//            return "fizz";
+//        }
+//
+//        if(multiple5){
+//            return "buzz";
+//        }
+//
+//        return number.toString();
+//    }
 
     public boolean multipleOf3(Integer number){
         return number % 3 == 0;
@@ -47,16 +50,22 @@ public class FizzBuzzSolution {
         return number % 5 == 0;
     }
 
-    public boolean hasGoalInIt(Integer number, int goal){
-        while(number > 0){
-            if(number % 10 == goal){
+    public boolean hasGoalInIt(Integer num, int goal){
+        Set<Integer> set = new HashSet<>();
+        while(true){
+            set.add(num / 10);
+        }
+        int num = (int) num;
+        while(num > 0){
+            if(num % 10 == goal){
                 return true;
             }
-            number = number / 10;
+            num = num / 10;
         }
         return false;
     }
 
 }
+
 
 
